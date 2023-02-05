76771c50bc577ed722359170e56d58948ad2b021
ztyang <ztyang@mit.edu>
2023-02-04 23:38:31 -0500

    plot on mac

--- mamao_tools/geometry_plotting.py
+++ mamao_tools/geometry_plotting.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import platform
 sns.set_style("darkgrid", {"axes.facecolor": ".9"})
 
 PAPER_VERSION = True
@@ -20,7 +21,7 @@ color_types = ['#3498db', '#e74c3c', '#2ecc71']
 training = [0.924, 0.882, 0.893, 0.881, 0.851, 0.886, 0.916] ## 0.886,
 testing = [0.89, 0.902, 0.868, 0.898, 0.894, 0.871, 0.916] ## 0.891,
 
-fig, ax = plt.subplots(figsize=(5, 5))
+fig, ax = plt.subplots(figsize=(4.5, 4.5))
 line = mlines.Line2D([0, 1], [0, 1], color='white')
 transform = ax.transAxes
 line.set_transform(transform)
@@ -40,7 +41,10 @@ fig.legend(handles, groups, ncol=1, fontsize=16,
 plt.tight_layout()
 
 if PAPER_VERSION:
-    plt.savefig('/home/yang/geometry.pdf', bbox_inches='tight')
+    if platform.system() == 'Darwin':
+        plt.savefig('geometry.pdf', bbox_inches='tight')
+    else:
+        plt.savefig('/home/yang/geometry.pdf', bbox_inches='tight')
     # plt.savefig('/home/yang/geometry.png', bbox_inches='tight')
 else:
     plt.show()
