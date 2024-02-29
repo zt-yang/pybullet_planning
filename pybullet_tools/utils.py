7242404ba363441655b70091594d4a9e9052e2ef
Yang <ztyang@mit.edu>
2024-02-29 15:22:53 -0500

    cleaning

--- pybullet_tools/utils.py
+++ pybullet_tools/utils.py
@@ -4031,9 +4031,9 @@ def check_initial_end(start_conf, end_conf, collision_fn, verbose=False):
     from pybullet_tools.bullet_utils import nice  ## YANG
     if collision_fn(start_conf, verbose=verbose):
         print(f'bullet.Warning: initial configuration {nice(start_conf)} is in collision')
-        set_renderer(True)
-        wait_unlocked()
-        collision_fn(start_conf, verbose=True)
+        # set_renderer(True)
+        # wait_unlocked()
+        # collision_fn(start_conf, verbose=True)
         return False
     if collision_fn(end_conf, verbose=verbose):
         print(f'bullet.Warning: end configuration {nice(end_conf)} is in collision')
